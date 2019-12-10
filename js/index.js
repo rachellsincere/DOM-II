6718de6ed7246da1e781b82f59c6061c9abd7bca
Rachell Sincere <rachellsincere@gmail.com>
2019-12-10 18:35:23 -0500

    added mouseenter and mouseout. installed gsap but didnt use it

--- js/index.js
+++ js/index.js
@@ -1,6 +1,40 @@
 // Your code goes here
+
+
+// click event
 const heading = document.querySelector(".logo-heading")
 heading.addEventListener("click", () => {
         heading.style.color = "red";
     })
  
+// Double Click event
+const intro = document.querySelector(".intro")
+intro.addEventListener("dblclick", () => {
+    intro.style.color = "purple";
+})
+
+
+// Mouseenter event
+const textContent = document.querySelector(".text-content")
+textContent.addEventListener("mouseenter", () => {
+    textContent.style.color = "orange";
+})
+
+// Mouseout event
+textContent.addEventListener("mouseout", () => {
+    textContent.style.color = "black";
+})
+//
+
+//
+
+//
+
+//
+
+//
+
+//
+
+
+
