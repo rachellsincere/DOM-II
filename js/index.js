2715a524b5ddb6fda0d567493e2afb508d86f6e7
Rachell Sincere <rachellsincere@gmail.com>
2019-12-10 20:47:20 -0500

    changed click event to links instead of header. Header has mouseover event instead. added mouse move event and scroll event

--- js/index.js
+++ js/index.js
@@ -1,23 +1,32 @@
 // Your code goes here
 
 
-// click event
+// Mouseover event
 const heading = document.querySelector(".logo-heading")
-heading.addEventListener("click", () => {
-        heading.style.color = "red";
+heading.addEventListener("mouseover", () => {
+        heading.innerHTML = "YOOOO";
     })
+
+// Mouseleave event
+heading.addEventListener("mouseleave", (e) => {
+    heading.innerHTML = "Fun Bus";
+    
+})
  
 // Double Click event
 const intro = document.querySelector(".intro")
 intro.addEventListener("dblclick", () => {
     intro.style.color = "purple";
+   
 })
 
 
 // Mouseenter event
 const textContent = document.querySelector(".text-content")
-textContent.addEventListener("mouseenter", () => {
+textContent.addEventListener("mouseenter", (e) => {
+    e.stopPropagation();
     textContent.style.color = "orange";
+
 })
 
 // Mouseout event
@@ -37,13 +46,25 @@ window.addEventListener("load", () => {
     alert("click and hover and see what you discover!");
 })
 
-//
+// Click event
+const navLinks = document.querySelector("a")
+navLinks.addEventListener("click", (e) => {
+    e.preventDefault();
+    navLinks.style.color = "red";
+})
 
-//
 
-//
+// Scroll event
+const body = document.querySelector(".container")
+window.addEventListener("scroll", () => {
+    body.style.background = "lavender";
+})
 
-//
+// Mouse move event
+const footer = document.querySelector(".footer")
+footer.addEventListener("mousemove", () => {
+    footer.style.background = "purple";
+})
 
 
 
